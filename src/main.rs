1bbe32c6481ebfd33493f789f114d0a4fd7270bb
James Langridge <l@ngridge.com>
2025-06-10 11:35:35 +0200

    Add is_face_card method

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ enum Suit {
     Clubs,
     Spades,
 }
-f
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Rank {
     Two,
     Three,
     Four,
-    Five,f
-    Six,f
+    Five,
+    Six,
     Seven,
     Eight,
     Nine,
@@ -29,6 +29,15 @@ enum Rank {
 struct Card {
     rank: Rank,
     suit: Suit
+}
+
+impl Card {
+    fn is_face_card(&self) -> bool {
+        match self.rank {
+            Rank::Jack | Rank::Queen | Rank::King => true,
+            _ => false,
+        }
+    }
 }
 
 fn main() {
@@ -55,4 +64,8 @@ fn main() {
 
     println!("Original: {:?}", ace_of_spades);
     println!("Copy: {:?}", my_card);
+
+    if king_of_hearts.is_face_card() {
+        println!("The king of Heards is indeed a face card.");
+    }
 }
